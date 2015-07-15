887d7dc04a18cd11654d4c793156d5f9bb3026f8
Till Freier <till.freier@combase.de>
2015-07-15 16:09:52 -0700

    notification manager
    Change-Id: Ia4447d631fe0b278d83fa7e347d8c90966b94698
    Signed-off-by: Till Freier <till.freier@combase.de>

---
+++ src/main/java/net/combase/desktopcrm/swing/NotificationManager.java
@@ -0,0 +1,126 @@
+/**
+ * 
+ */
+package net.combase.desktopcrm.swing;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import net.combase.desktopcrm.data.CrmHelper;
+import net.combase.desktopcrm.data.CrmManager;
+import net.combase.desktopcrm.data.DataStoreManager;
+import net.combase.desktopcrm.domain.AbstractCrmObject;
+import net.combase.desktopcrm.domain.Call;
+import ch.swingfx.twinkle.NotificationBuilder;
+import ch.swingfx.twinkle.event.NotificationEvent;
+import ch.swingfx.twinkle.event.NotificationEventAdapter;
+
+/**
+ * @author "Till Freier"
+ *
+ */
+public class NotificationManager
+{
+	private static Timer timer = null;
+
+	public static synchronized void init()
+	{
+		if (timer != null)
+			throw new RuntimeException("notification manager is already initialized");
+
+		timer = new Timer(true);
+
+		initActionTimer();
+		initCallTimer();
+	}
+
+	/**
+	 * 
+	 */
+	private static void initActionTimer()
+	{
+		timer.schedule(new TimerTask()
+		{
+			@Override
+			public void run()
+			{
+				List<AbstractCrmObject> leadList = CrmHelper.getActionObjects();
+				for (final AbstractCrmObject lead : leadList)
+				{
+					System.out.println("request action item for " + lead.getTitle());
+					String msg = "'" + lead.getTitle() +
+						"' has no planned actions. Click here to schedule a task.";
+
+					NotificationBuilder nb = DesktopUtil.createNotificationBuilder();
+					nb.withTitle("Plan Follow Up Action");
+					nb.withMessage(msg);
+					nb.withIcon(CrmIcons.WARN);
+					nb.withDisplayTime(60000);
+
+					nb.withListener(new NotificationEventAdapter()
+					{
+						@Override
+						public void clicked(NotificationEvent event)
+						{
+							DesktopUtil.openBrowser(lead.getViewUrl());
+						}
+					});
+
+					nb.showNotification();
+
+					// wait 2 minutes
+					try
+					{
+						Thread.sleep(180000);
+					}
+					catch (InterruptedException e1)
+					{
+						e1.printStackTrace();
+					}
+				}
+			}
+		}, 3000, 300000);
+	}
+
+	/**
+	 * 
+	 */
+	private static void initCallTimer()
+	{
+		timer.schedule(new TimerTask()
+		{
+			@Override
+			public void run()
+			{
+				System.out.println("Check for calls...");
+
+				if (DataStoreManager.getSettings().isCallReminder())
+				{
+					final Call c = CrmManager.getUpcomingCall();
+					if (c != null)
+					{
+						String msg = "You have an upcoming call: " + c.getTitle();
+
+						NotificationBuilder nb = DesktopUtil.createNotificationBuilder();
+						nb.withTitle("Upcoming Call");
+						nb.withMessage(msg);
+						nb.withIcon(CrmIcons.CALL);
+						nb.withDisplayTime(30000);
+
+						nb.withListener(new NotificationEventAdapter()
+						{
+							@Override
+							public void clicked(NotificationEvent event)
+							{
+								DesktopUtil.openBrowser(c.getViewUrl());
+							}
+						});
+
+						nb.showNotification();
+					}
+				}
+			}
+		}, 5000, 120000);
+	}
+}
--- src/main/java/net/combase/desktopcrm/swing/SwingWindow.java
+++ src/main/java/net/combase/desktopcrm/swing/SwingWindow.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -13,128 +12,16 @@ import javax.swing.JMenuItem;
 import javax.swing.UIManager;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
-import net.combase.desktopcrm.data.CrmHelper;
-import net.combase.desktopcrm.data.CrmManager;
-import net.combase.desktopcrm.data.DataStoreManager;
-import net.combase.desktopcrm.domain.AbstractCrmObject;
-import net.combase.desktopcrm.domain.Call;
-import ch.swingfx.twinkle.NotificationBuilder;
-import ch.swingfx.twinkle.event.NotificationEvent;
-import ch.swingfx.twinkle.event.NotificationEventAdapter;
-
 public class SwingWindow
 {
 	private JFrame frame;
 
-	private static final Runnable actionRequiredCheckRunner = new Runnable()
-	{
-
-		@Override
-		public void run()
-		{
-			List<AbstractCrmObject> leadList = CrmHelper.getActionObjects();
-			for (final AbstractCrmObject lead : leadList)
-			{
-				System.out.println("request action item for " + lead.getTitle());
-				String msg = "'" + lead.getTitle() +
-					"' has no planned actions. Click here to schedule a task.";
-
-				NotificationBuilder nb = DesktopUtil.createNotificationBuilder();
-				nb.withTitle("Plan Follow Up Action");
-				nb.withMessage(msg);
-				nb.withIcon(CrmIcons.WARN);
-				nb.withDisplayTime(60000);
-
-				nb.withListener(new NotificationEventAdapter()
-				{
-					@Override
-					public void clicked(NotificationEvent event)
-					{
-						DesktopUtil.openBrowser(lead.getViewUrl());
-					}
-				});
-
-				nb.showNotification();
-
-				// wait 2 minutes
-				try
-				{
-					Thread.sleep(180000);
-				}
-				catch (InterruptedException e1)
-				{
-					e1.printStackTrace();
-				}
-			}
-			// wait 5 minutes
-			try
-			{
-				Thread.sleep(300000);
-			}
-			catch (InterruptedException e1)
-			{
-				e1.printStackTrace();
-			}
-
-			actionRequiredCheckRunner.run();
-		}
-	};
-
-	private static final Runnable callCheckRunner = new Runnable()
-	{
-
-		@Override
-		public void run()
-		{
-			System.out.println("Check for calls...");
-
-			if (DataStoreManager.getSettings().isCallReminder())
-			{
-				final Call c = CrmManager.getUpcomingCall();
-				if (c != null)
-				{
-					String msg = "You have an upcoming call: " + c.getTitle();
-
-					NotificationBuilder nb = DesktopUtil.createNotificationBuilder();
-					nb.withTitle("Upcoming Call");
-					nb.withMessage(msg);
-					nb.withIcon(CrmIcons.CALL);
-					nb.withDisplayTime(30000);
-
-					nb.withListener(new NotificationEventAdapter()
-					{
-						@Override
-						public void clicked(NotificationEvent event)
-						{
-							DesktopUtil.openBrowser(c.getViewUrl());
-						}
-					});
-
-					nb.showNotification();
-				}
-			}
-			// wait 2 minutes
-			try
-			{
-				Thread.sleep(120000);
-			}
-			catch (InterruptedException e1)
-			{
-				e1.printStackTrace();
-			}
-
-			callCheckRunner.run();
-		}
-	};
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args)
 	{
-// NotificationBuilder n = DesktopUtil.createNotificationBuilder();
-// n.withTitle("Test").withMessage("Hello").showNotification();
-
 		EventQueue.invokeLater(new Runnable()
 		{
 			@Override
@@ -217,8 +104,7 @@ public class SwingWindow
 
 		frame.setIconImage(CrmIcons.USER.getImage());
 
-		new Thread(actionRequiredCheckRunner).start();
-		new Thread(callCheckRunner).start();
+		NotificationManager.init();
 	}
 
 }
