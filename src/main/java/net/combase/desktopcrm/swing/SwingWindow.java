c7e5ee7639dbf4832ce1caddcc0cf46bec528797
Till Freier <till.freier@combase.de>
2015-07-17 18:48:52 -0700

    exit prompt
    Change-Id: I9401d2ad5ddae6573a0530ccca2aa9eb0925e604

--- src/main/java/net/combase/desktopcrm/swing/SwingWindow.java
+++ src/main/java/net/combase/desktopcrm/swing/SwingWindow.java
@@ -4,11 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
@@ -67,7 +70,7 @@ public class SwingWindow
 
 		frame = new JFrame("Sugar CRM");
 		frame.setBounds(100, 100, 600, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
@@ -125,6 +128,25 @@ public class SwingWindow
 		frame.getContentPane().add(tabbedPane);
 
 		frame.setIconImage(CrmIcons.USER.getImage());
+
+		frame.addWindowListener(new WindowAdapter()
+		{
+
+			@Override
+			public void windowClosing(WindowEvent we)
+			{
+				String ObjButtons[] = { "Yes", "No" };
+				int PromptResult = JOptionPane.showOptionDialog(null,
+					"Are you sure you want to exit? You will no longer receive CRM notification if this window is closed.",
+					"CRM Notifications",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, ObjButtons,
+					ObjButtons[1]);
+				if (PromptResult == 0)
+				{
+					System.exit(0);
+				}
+			}
+		});
 
 		NotificationManager.init();
 	}
