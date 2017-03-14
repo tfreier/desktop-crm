50a8e0a8380ad3d76b22b43cfeecc636b3d11d68
Till Freier <till.freier@combase.de>
2017-03-14 14:56:34 -0700

    keep selection in task list

--- src/main/java/net/combase/desktopcrm/swing/TaskTablePanel.java
+++ src/main/java/net/combase/desktopcrm/swing/TaskTablePanel.java
@@ -23,13 +23,17 @@ import net.combase.desktopcrm.data.CrmManager;
 import net.combase.desktopcrm.domain.Lead;
 import net.combase.desktopcrm.domain.Task;
 
+
+
 public class TaskTablePanel extends JPanel
 {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6149463410211475900L;
+
 	private JTable table;
+
 
 	/**
 	 * Create the panel.
@@ -38,41 +42,36 @@ public class TaskTablePanel extends JPanel
 	{
 		setLayout(new BorderLayout(0, 0));
 
-
 		final TaskTableModel model = new TaskTableModel(new ArrayList<Task>());
 
 		table = new JTable(model);
 
 		// enable button clicks
-		table.addMouseListener(new MouseAdapter()
-		{
+		table.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e)
 			{
 				int column = table.getColumnModel().getColumnIndexAtX(e.getX());
 				int row = e.getY() / table.getRowHeight();
 
-				if (row < table.getRowCount() && row >= 0 && column < table.getColumnCount() &&
-					column >= 0)
+				if (row < table.getRowCount() && row >= 0 && column < table.getColumnCount() && column >= 0)
 				{
 					Object value = table.getValueAt(row, column);
 					if (value instanceof JButton)
 					{
-						((JButton)value).doClick();
+						((JButton) value).doClick();
 					}
 				}
 			}
 		});
 
-		table.setDefaultRenderer(JButton.class, new TableCellRenderer()
-		{
+		table.setDefaultRenderer(JButton.class, new TableCellRenderer() {
 			@Override
-			public Component getTableCellRendererComponent(JTable table, Object value,
-				boolean isSelected, boolean hasFocus, int row, int column)
+			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
 			{
 				if (value instanceof JButton)
 				{
-					return (JButton)value;
+					return (JButton) value;
 				}
 
 				return new JLabel();
@@ -92,12 +91,11 @@ public class TaskTablePanel extends JPanel
 		add(table.getTableHeader(), BorderLayout.NORTH);
 		add(new JScrollPane(table), BorderLayout.CENTER);
 
-		table.addMouseListener(new MouseAdapter()
-		{
+		table.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent me)
 			{
-				JTable table = (JTable)me.getSource();
+				JTable table = (JTable) me.getSource();
 				if (me.getClickCount() == 2)
 				{
 					Point p = me.getPoint();
@@ -107,24 +105,24 @@ public class TaskTablePanel extends JPanel
 					String parentId = task.getParentId();
 					switch (task.getParentType())
 					{
-						case "Cases" :
+						case "Cases":
 							title = CrmManager.getCase(parentId).getTitle();
 							break;
-						case "Leads" :
+						case "Leads":
 							Lead lead = CrmManager.getLead(parentId);
 							title = lead.getTitle();
 							title += " - " + lead.getAccountName();
 							break;
-						case "Contacts" :
+						case "Contacts":
 							title = CrmManager.getContact(parentId).getTitle();
 							break;
-						case "Accounts" :
+						case "Accounts":
 							title = CrmManager.getCase(parentId).getTitle();
 							break;
-						case "Opportunities" :
+						case "Opportunities":
 							title = CrmManager.getOpprtunity(parentId).getTitle();
 							break;
-						default :
+						default:
 							break;
 					}
 					StringSelection stringSelection = new StringSelection(title);
@@ -135,16 +133,28 @@ public class TaskTablePanel extends JPanel
 			}
 		});
 
-
 		java.util.Timer t = new java.util.Timer(true);
-		t.schedule(new TimerTask()
-		{
+		t.schedule(new TimerTask() {
 			@Override
 			public void run()
 			{
 				List<Task> updatedList = CrmManager.getTaskList();
 				System.out.println(updatedList);
+				int r = table.getSelectedRow();
+				Task t = null;
+				if (r >= 0)
+					t = model.getTask(r);
 				model.update(updatedList);
+				if (t != null)
+				{
+					int i = 0;
+					for (Task task : updatedList)
+					{
+						if (task.getId().equals(t.getId()))
+							table.setRowSelectionInterval(i, i);
+						i++;
+					}
+				}
 			}
 		}, 500, 120000);
 	}
